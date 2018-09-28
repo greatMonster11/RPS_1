33e2fe5a02ef9d887ed61511db2766f650d185b4
greatMonster11 <nguyenphuocthanhtn2015@gmai.com>
2018-09-28 08:36:10 +0700

    create a play ground

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const PlayerCard = ({color, symbol}) => {
+	const style = {
+		backgroundColor: color
+	}
+	return (
+		<div style={style} className="player-card">
+			{symbol}
+		</div>
+	)
+}
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<PlayerCard 
+					color="red"
+					symbol="paper" />
+				<PlayerCard 
+					color="blue"
+					symbol="rock" />
 			</div>
 		);
 	}
