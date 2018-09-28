b6e2bb0418f8b94be37619764fe958b6350ba1dd
greatMonster11 <nguyenphuocthanhtn2015@gmai.com>
2018-09-28 10:11:55 +0700

    complete the game

--- src/App.js
+++ src/App.js
@@ -3,24 +3,63 @@ import './App.css';
 
 const PlayerCard = ({color, symbol}) => {
 	const style = {
-		backgroundColor: color
+		backgroundColor: color,
+		backgroundImage: "url(./img/" + symbol + ".png)" 
 	}
 	return (
 		<div style={style} className="player-card">
 			{symbol}
-		</div>
+		</div>		
 	)
 }
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.symbol=["rock", "paper", "scissors"]
+		this.state = {}
+	}
+
+	decideWinner = () => {
+		const {playerBlue, playerRed} = this.state
+		if(playerBlue === playerRed) {
+			return "It's a draw"
+		}
+		if((playerBlue === "rock" && playerRed === "scissors") ||
+				(playerBlue === "paper" && playerRed === "rock")
+				(playerBlue === "scissors" && playerRed === "paper")) {
+					return "Player Blue is a winner !"
+				}
+		else return "Player Red is a winner !"
+	}
+
+	runGame = () => {
+		/* const index = Math.floor(Math.random()*3)
+		console.log(this.symbol[index]) */
+		let counter = 0
+		let myInterval = setInterval(() => {
+			counter++
+			this.setState({
+				playerRed: this.symbol[Math.floor(Math.random()*3)],
+				playerBlue: this.symbol[Math.floor(Math.random()*3)],
+				winner: ""
+			})
+			if(counter > 40) { 
+				clearInterval(myInterval) 
+				this.setState({winner: this.decideWinner})
+			}
+		}, 100)
+	}
 	render() {
 		return (
 			<div className="App">
 				<PlayerCard 
 					color="red"
-					symbol="paper" />
-				<PlayerCard 
+					symbol={this.state.playerRed} />
+				<PlayerCard 	
 					color="blue"
-					symbol="rock" />
+					symbol={this.state.playerBlue} />
+				<p>{this.state.winner}</p>
+				<button onClick={this.runGame}>Run game</button>
 			</div>
 		);
 	}
